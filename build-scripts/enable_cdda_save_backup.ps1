a2a68739b2a9fd8aa9a346f70f54581b59ffa581
Zugfritze <70323844+Zugfritze@users.noreply.github.com>
2023-09-16 22:49:09 +0800

    添加存档备份功能

---
+++ build-scripts/enable_cdda_save_backup.ps1
@@ -0,0 +1,15 @@
+$OriginalDirectory = Get-Location
+$CDDASaveBackupgitpath = $env:ENABLE_CDDA_SAVE_BACKUP
+if (-not ($null -eq $CDDASaveBackupgitpath)) {
+    Invoke-Expression -Command "git clone --depth=1 https://github.com/Zugfritze/CDDA-Save-Backup $CDDASaveBackupgitpath"
+    Set-Location -Path $CDDASaveBackupgitpath
+    $env:RUSTFLAGS = '-C target-feature=+crt-static'
+    Invoke-Expression -Command "cargo build --profile release"
+    Copy-Item -Path "$CDDASaveBackupgitpath\target\cxxbridge\cdda_save_backup\src\lib.rs.h" -Destination "$OriginalDirectory\src\cdda_save_backup.h"
+    Copy-Item -Path "$CDDASaveBackupgitpath\target\cxxbridge\cdda_save_backup\src\lib.rs.cc" -Destination "$OriginalDirectory\src\cdda_save_backup.cpp"
+    Set-Location -Path $OriginalDirectory
+}
+else {
+    Write-Host "------错误------"
+    Write-Host "环境变量 ENABLE_CDDA_SAVE_BACKUP 为空"
+}
